4f20e8a8e2dc06b8a9e0c24faf98ffa0d4965ca4
ByMykel <bymykel5@gmail.com>
2023-12-24 19:46:41 +0100

    refactor: cleanup

--- src/services/Cs2Service.ts
+++ src/services/Cs2Service.ts
@@ -1,7 +1,35 @@
-import BaseService from "./BaseService"
+import axios from "axios";
 
-export default class Cs2Service extends BaseService {
-    constructor() {
-        super("https://cs2-api.vercel.app/api")
+export default class BaseService {
+    protected baseUrl: string = "https://cs2-api.vercel.app/api";
+
+    async getItems(query: string) {
+        const url = `${this.baseUrl}/items?q=${encodeURIComponent(query)}`;
+
+        const response = await axios.get(url);
+
+        return response.data;
+    }
+
+    async getById(id: string) {
+        const url = `${this.baseUrl}/items?id=${id}`;
+
+        const response = await axios.get(url);
+
+        return response.data;
+    }
+
+    async search(query: string) {
+        const url = `${this.baseUrl}/search?q=${encodeURIComponent(query)}`;
+
+        const response = await axios.get(url);
+
+        return response.data;
+    }
+
+    async getNext(next: string) {
+        const response = await axios.get(next);
+
+        return response.data;
     }
 }
