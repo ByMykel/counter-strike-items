3e2d56428271c59d86bb3562669a86c3777fc627
ByMykel <bymykel5@gmail.com>
2024-03-23 16:46:08 +0100

    feat: test PWA.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,38 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
+import { VitePWA } from "vite-plugin-pwa"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/counter-strike-items/',
-  plugins: [vue()],
+    base: "/counter-strike-items/",
+    plugins: [
+        vue(),
+        VitePWA({
+            registerType: "autoUpdate",
+            includeAssets: [
+                "favicon.ico",
+                "apple-touch-icon.png",
+                "mask-icon.svg"
+            ],
+            manifest: {
+                name: "counter-strike-items",
+                short_name: "cs-items",
+                description:
+                    "Browse all CS2 and CS:GO skins, knives, gloves, cases, collections, stickers, music kits, and more.",
+                theme_color: "#FF5E65",
+                icons: [
+                    {
+                        src: "pwa-192x192.png",
+                        sizes: "192x192",
+                        type: "image/png"
+                    },
+                    {
+                        src: "pwa-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png"
+                    }
+                ]
+            }
+        })
+    ]
 })
