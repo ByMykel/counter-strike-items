f2157dba10de1249d92126b8fab7e21fc7bab083
ByMykel <bymykel5@gmail.com>
2024-03-23 17:05:59 +0100

    feat: notify on new updates.

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import { createApp } from "vue"
 import { createPinia } from "pinia"
+import { registerSW } from "virtual:pwa-register"
 import "./style.css"
 import App from "./App.vue"
 
 const pinia = createPinia()
 const app = createApp(App)
 import router from "./router"
+
+const updateSW = registerSW({
+    onNeedRefresh() {
+        if (confirm("A new update is available. Do you want to update now?")) {
+            updateSW()
+        }
+    }
+})
 
 app.use(router)
 app.use(pinia)
