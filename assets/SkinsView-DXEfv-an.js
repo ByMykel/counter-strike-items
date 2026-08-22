e4a0183fd57e2eb63a647befaab9bef1b9f5c3e5
ByMykel <bymykel5@gmail.com>
2026-08-22 07:27:30 +0000

    Deploying to gh-pages from @ ByMykel/counter-strike-items@de2a3e7d242041fd1e29a0615f0a11b837c85221 🚀

---
+++ assets/SkinsView-DXEfv-an.js
@@ -0,0 +1,1 @@
+import{$ as e,Et as t,I as n,U as r}from"./ui-vendor-C5i1XdHv.js";import{t as i}from"./BaseItemListView-DLk5JYUS.js";import{t as a}from"./SkinService-Dv_c72Mi.js";var o=r({__name:`SkinsView`,setup(r){let o=new a().query;return(r,a)=>(e(),n(i,{query:t(o),"store-id":`skins`,title:`Skins - Counter-Strike items`},null,8,[`query`]))}});export{o as default};
