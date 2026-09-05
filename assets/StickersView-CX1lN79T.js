e82b8559236936c03e7af69c380169d060419afe
ByMykel <bymykel5@gmail.com>
2026-09-05 06:54:46 +0000

    Deploying to gh-pages from @ ByMykel/counter-strike-items@6281c79fd6feb953c5bf31927de17a295216f6b3 🚀

---
+++ assets/StickersView-CX1lN79T.js
@@ -0,0 +1,1 @@
+import{$ as e,Et as t,I as n,U as r}from"./ui-vendor-_g3KZWuv.js";import{t as i}from"./BaseItemListView-Bjzx9jhe.js";import{t as a}from"./StickersService-Dl957R5-.js";var o=r({__name:`StickersView`,setup(r){let o=new a().query;return(r,a)=>(e(),n(i,{query:t(o),"store-id":`stickers`,title:`Stickers - Counter-Strike items`},null,8,[`query`]))}});export{o as default};
